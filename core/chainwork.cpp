214fd11badd8c9b6a432a05a498cb7fbb06463e7
TheNerd <dennis.dentremont@gmail.com>
2022-10-19 16:15:15 -0300

    updated copyright info

--- core/chainwork.cpp
+++ core/chainwork.cpp
@@ -1,4 +1,5 @@
 ﻿// Copyright 2018 The Beam Team
+// Copyright 2019 - 2022 The LiteCash Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
